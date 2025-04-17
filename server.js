f99468d91383ccb608d3c809765b148f1eb7546a
Marcelo Moura <134522050+m-marcelo@users.noreply.github.com>
2025-04-17 10:37:06 -0300

    Update server.js
    Ajuste na porta 3000.  Iremos subir o projeto no Render e a plataforma usa porta dinâmica, sendo assim não podemos deixar a porta com um valor fixo(3000)

--- server.js
+++ server.js
@@ -54,6 +54,11 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(3000, () => {
+/*server.listen(3000, () => {
     console.log('Servidor rodando em http://localhost:3000');
+});*/
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
